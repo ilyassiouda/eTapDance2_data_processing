ac9f5c22c2dd317638da7fe58d77ba348ebb015e
Ilyas Siouda <42919204+ilyassiouda@users.noreply.github.com>
2020-12-08 16:02:10 +0100

    Add files via upload

---
+++ Jupyter_notebook_ml_env_fixe.bat
@@ -0,0 +1,5 @@
+call "C:\Users\ilyas\Anaconda3\Scripts\activate.bat"
+cd "C:\Users\ilyas\Google Drive\Hepia\Troisième année  MT 2019-2020\Projet et gestion\Projet de bachelor\Annexes\Python"
+call activate ml
+call jupyter notebook
+pause
---
+++ Jupyter_notebook_ml_env_petit_PC.bat
@@ -0,0 +1,5 @@
+call "C:\Users\Anja\Anaconda3\Scripts\activate.bat"
+cd "C:\Users\Anja\Google Drive\Hepia\Troisième année  MT 2019-2020\Projet et gestion\Projet de bachelor\Annexes\Python"
+call activate ml
+call jupyter notebook
+pause
